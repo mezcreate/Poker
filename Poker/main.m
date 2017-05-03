2c12edd11d5e2d19610772736f92da9ed20bd5eb
meng Meng <mengzhiqi@mengzhiqidemacbook-pro.local>
2017-05-03 21:20:38 +0800

    Initial Commit

---
+++ Poker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Poker
+//
+//  Created by Mengzhiqi on 17/5/3.
+//  Copyright © 2017年 Mengzhiqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
